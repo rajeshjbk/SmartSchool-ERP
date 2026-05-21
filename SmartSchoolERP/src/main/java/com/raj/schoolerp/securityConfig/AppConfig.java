90885f970842d1df78f2f7adfe67b32a9151caee
Rajesh kumar <rajeshkumarkarri3434@gmail.com>
2026-05-21 12:15:40 +0530

    Fix JWT secret config

--- SmartSchoolERP/src/main/java/com/raj/schoolerp/securityConfig/AppConfig.java
+++ SmartSchoolERP/src/main/java/com/raj/schoolerp/securityConfig/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -190,7 +191,7 @@ public class AppConfig {
 						new JwtTokenValidatorFilter(),
 						BasicAuthenticationFilter.class)
 
-				.httpBasic(httpBasic -> httpBasic.disable())
+				.httpBasic(Customizer.withDefaults())
 
 				.formLogin(form -> form.disable());
 
--- SmartSchoolERP/src/main/java/com/raj/schoolerp/securityConfig/SecurityConstants.java
+++ SmartSchoolERP/src/main/java/com/raj/schoolerp/securityConfig/SecurityConstants.java
@@ -2,9 +2,10 @@ package com.raj.schoolerp.securityConfig;
 
 public class SecurityConstants {
 
-	public static final String JWT_KEY = System.getenv("JWT_SECRET");
-//	public static final String JWT_KEY =
-//			"secretRAJESHITJRTPSCHOOLERPJwtImplementation";
-	public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_KEY =
+            System.getenv("JWT_SECRET") != null
+            ? System.getenv("JWT_SECRET")
+            : "secretRAJESHITJRTPSCHOOLERPJwtImplementation";
 
+    public static final String JWT_HEADER = "Authorization";
 }
